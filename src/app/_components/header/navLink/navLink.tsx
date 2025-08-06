38c5f06c80934686afb97c0cf760ad8fbb00f11b
Faramarz <www.faramarz1991f.a@gmail.com>
2025-08-06 18:20:30 +0330

    new add

--- src/app/_components/header/navLink/navLink.tsx
+++ src/app/_components/header/navLink/navLink.tsx
@@ -18,8 +18,8 @@ export const NavLink = ({
       href={href}
       className={`group relative m-2 w-[80px] text-start text-md transition-colors duration-100
         ${isActive
-          ? "font-bold text-amber-600 border-b border-amber-600 shadow-2 shadow-amber-600 dark:text-pink-400 dark:border-pink-400 scale-y-125"
-          : "text-shop-gray-900 dark:text-gray-100 hover:text-amber-600 dark:hover:text-pink-400"}
+          ? "font-bold text-amber-600 border-b border-amber-600 shadow-2 shadow-amber-600  scale-y-125"
+          : "text-shop-gray-900  hover:text-amber-600 "}
         lg:mt-0 lg:text-center`}
     >
       {children}
@@ -27,7 +27,7 @@ export const NavLink = ({
       {/* فقط وقتی لینک فعال نیست افکت نمایشی زیرش بیاد */}
       {!isActive && (
         <span
-          className="absolute bottom-0 left-0 h-[2px] w-full bg-amber-600 dark:bg-pink-400
+          className="absolute bottom-0 left-0 h-[2px] w-full bg-amber-600 
           transform scale-x-0 origin-left transition-transform duration-500 group-hover:scale-x-100"
         ></span>
       )}
